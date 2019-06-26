59b68fab571449c9690dc586d130f29196ff74e4
Pavel Macenauer <pavel.macenauer@nxp.com>
2019-06-26 13:42:10 +0200

    [AIR-1468] Fixed compile issue when debug=1

--- src/core/CL/CLKernelLibrary.cpp
+++ src/core/CL/CLKernelLibrary.cpp
@@ -26,6 +26,7 @@
 #include "arm_compute/core/CL/CLHelpers.h"
 #include "arm_compute/core/Error.h"
 #include "arm_compute/core/Utils.h"
+#include "arm_compute/core/Log.h"
 
 #include <algorithm>
 #include <fstream>
